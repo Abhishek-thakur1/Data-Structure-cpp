9dd4e985b52fdde682cfd40deda7c4fb014a985f
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-08-26 13:31:32 +0530

    format

--- tree/basic_implementation_BST/BST.cpp
+++ tree/basic_implementation_BST/BST.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<stdexcept>
+#include <stdexcept>
 #include "BST.h"
 
 using namespace std;
@@ -8,108 +8,129 @@ using namespace std;
 
 BST::~BST() {}
 
-
 // for inserting elements in the tree
-void BST::insertNode(int data){
-    Node * newNode = new Node();
-    newNode->data = data;
-    newNode->left = newNode->right = NULL;
+void BST::insertNode(int data)
+{
+	Node *newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
 
-    insert(root, newNode);
+	insert(root, newNode);
 
-    size++;
+	size++;
 }
 
-void BST::insert(Node *&root, Node *&newNode){
+void BST::insert(Node *&root, Node *&newNode)
+{
 
-    if(root == NULL) root = newNode;
+	if (root == NULL)
+		root = newNode;
 
-    
 	else if (newNode->data < root->data)
-    {
-        insert(root->left, newNode);
-    }
-    else
-    {
-        insert(root->right, newNode);
-    }
+	{
+		insert(root->left, newNode);
+	}
+	else
+	{
+		insert(root->right, newNode);
+	}
 }
 
 // for printing elements
-void BST::print() {
-	Node* temp = root;
+void BST::print()
+{
+	Node *temp = root;
 	cout << endl;
-	cout << endl << "Printing data in BST..." << endl;
+	cout << endl
+		 << "Printing data in BST..." << endl;
 	DisplayInOrder(temp);
 }
 
-void BST::DisplayInOrder(Node* temp) {
-	if (root == NULL) {
-		cout << endl << "List is empty. Nothing to display." << endl;
+void BST::DisplayInOrder(Node *temp)
+{
+	if (root == NULL)
+	{
+		cout << endl
+			 << "List is empty. Nothing to display." << endl;
 		return;
 	}
 
-	if (temp->left != NULL) {
+	if (temp->left != NULL)
+	{
 		DisplayInOrder(temp->left);
 	}
-	
+
 	cout << temp->data << endl;
-	
-	if (temp->right != NULL) {
+
+	if (temp->right != NULL)
+	{
 		DisplayInOrder(temp->right);
 	}
 
 	// cout << temp->data;
 }
 
-
 // for searching elements
-void BST::search(int data) {
-	Node* temp = root;
+void BST::search(int data)
+{
+	Node *temp = root;
 	searchNode(temp, data);
 }
 
-void BST::searchNode(Node* temp, int data) {
-	if (root == NULL) {
-		cout << endl << "List is empty. No items to match." << endl << endl;
+void BST::searchNode(Node *temp, int data)
+{
+	if (root == NULL)
+	{
+		cout << endl
+			 << "List is empty. No items to match." << endl
+			 << endl;
 		return;
 	}
 
-	if (temp->left != NULL) {
+	if (temp->left != NULL)
+	{
 		searchNode(temp->left, data);
 	}
 
-	if (temp->data == data) {
-		cout << endl << "Search Operation: Node with value " << data << " found." << endl;
+	if (temp->data == data)
+	{
+		cout << endl
+			 << "Search Operation: Node with value " << data << " found." << endl;
 		return;
 	}
 
-	if (temp->right != NULL) {
+	if (temp->right != NULL)
+	{
 		searchNode(temp->right, data);
 	}
 }
 
 // size of the tree
-void BST::printSize(){
-	cout << endl << "Size of BST is " << size << " nodes." << endl;
+void BST::printSize()
+{
+	cout << endl
+		 << "Size of BST is " << size << " nodes." << endl;
 }
-
 
 // MINIMUM element in the tree
-void BST::printMIN(){
+void BST::printMIN()
+{
 	Node *tmp = root;
-	cout<< "Minimum Element in the TREE: " << findMin(tmp) << endl;
+	cout << "Minimum Element in the TREE: " << findMin(tmp) << endl;
 }
 
-int BST::findMin(Node *tmp){
-	try{
+int BST::findMin(Node *tmp)
+{
+	try
+	{
 		while (tmp->left != NULL)
 		{
 			tmp = tmp->left;
 		}
 		return tmp->data;
 	}
-	catch (const invalid_argument &e){
+	catch (const invalid_argument &e)
+	{
 		if (tmp == NULL)
 		{
 			throw invalid_argument("Tree is Empty");
@@ -118,21 +139,26 @@ int BST::findMin(Node *tmp){
 	}
 }
 
-
 // MAXIMUM element in the tree
-void BST::printMAX(){
+void BST::printMAX()
+{
 	Node *tmp = root;
 	cout << "Maximum Element in the TREE: " << findMax(tmp) << endl;
 }
-int BST::findMax(Node *tmp){
-	try{
-		while (tmp->right != NULL){
+int BST::findMax(Node *tmp)
+{
+	try
+	{
+		while (tmp->right != NULL)
+		{
 			tmp = tmp->right;
 		}
 		return tmp->data;
 	}
-	catch (const invalid_argument &e){
-		if (tmp == NULL){
+	catch (const invalid_argument &e)
+	{
+		if (tmp == NULL)
+		{
 			throw invalid_argument("Tree is Empty");
 		}
 		cerr << "ERROR: " << e.what() << endl;
